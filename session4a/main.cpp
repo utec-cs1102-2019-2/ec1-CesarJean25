6f72dbf15df6e51450d2a5377336ad9548e6319c
CesarJean25 <55211908+CesarJean25@users.noreply.github.com>
2019-09-11 17:00:15 -0500

    Ejemplo de punteros

--- session4a/main.cpp
+++ session4a/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+int ival = 42;
+int*pival = &ival;
+std::cout << pival << "\t" << *pival << "\t" << &pival;
     return 0;
 }
