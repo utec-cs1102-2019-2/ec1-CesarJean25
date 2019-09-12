c3a0ce939cf30fbdcb2f2bf7b7908a38919d5339
CesarJean25 <55211908+CesarJean25@users.noreply.github.com>
2019-09-12 09:30:09 -0500

    commit session 4b

---
+++ session 4b/cmake-build-debug/CMakeFiles/session_4b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session_4b.dir/main.cpp.obj"
+  "session_4b.pdb"
+  "session_4b.exe"
+  "session_4b.exe.manifest"
+  "session_4b.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session_4b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
